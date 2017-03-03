7161ee6616331b7eb1dc68673b7e3b185c88500f
Mario <yordanov.mario@yahoo.com>
2017-03-03 23:04:34 +0200

    ädded class user and demo

---
+++ freeAgents/demo.java
@@ -0,0 +1,11 @@
+package freeAgents;
+
+public class demo {
+
+	public static void main(String[] args) {
+		
+		User user = new User("zz", "M123456o", "yordanov.mario@yahoo.com", "mario1", "ddd");
+		
+	}
+
+}
